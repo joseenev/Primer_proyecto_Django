5a3fcff1458d0dd996e0088846400a4ed6c17243
joseenev <131764882+joseenev@users.noreply.github.com>
2023-05-05 09:35:21 +0200

    Update views.py

--- Proyecto1/views.py
+++ Proyecto1/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 def saludo(request):
     
     return HttpResponse("Hola Jose, estás aprendiendo Django")
-
+#Una segunda vista que estará en otra url
 def despedida(request):
     
     return HttpResponse("Hasta luego, seguiremos aprendiendo")
